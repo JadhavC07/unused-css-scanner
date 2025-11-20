3f50ff79efd425cb6c60a5cc055454ea644eda41
JadhavC07 <cjaddhav@gmail.com>
2025-11-20 18:15:22 +0530

    initial commit

---
+++ example/test-file.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const TestComponent = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+  },
+  unusedStyle: {
+    color: 'red',
+  },
+  anotherUnused: {
+    padding: 10,
+  },
+});
+
+export default TestComponent;
